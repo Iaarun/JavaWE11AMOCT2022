c2f9d4f6b4347dad3cc0408602ad0a6e7655e069
Iaarun <automationbyaarun@gmail.com>
2023-02-05 11:07:44 +0530

    seleniumscripts

--- src/seleniumTestScripts/SelScripts.java
+++ src/seleniumTestScripts/SelScripts.java
@@ -1,20 +1,112 @@
 package seleniumTestScripts;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
 
 public class SelScripts {
 
 	WebDriver driver;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		SelScripts sel = new SelScripts();
 		sel.launchBrowser("chrome");
-		sel.fileUpload();
+		sel.handleScreenshot();
+
 		// sel.closeBrowser();
+
+	}
+	public void handleScreenshot() throws IOException {
+		driver.get("https://jqueryui.com/droppable/");
+		driver.switchTo().frame(0);
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+		
+		Actions action = new Actions(driver);
+		
+		//action.clickAndHold(drag).moveToElement(drop).release().build().perform();
+		action.dragAndDrop(drag, drop).perform();
+		
+	//capture screenshot
+	 File file=	((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	 FileHandler.copy(file, new File("D:\\screenshot\\dd.jpeg"));
+	}
+
+	public void handleDragdrop() {
+		driver.get("https://jqueryui.com/droppable/");
+		driver.switchTo().frame(0);
+		WebElement drag = driver.findElement(By.id("draggable"));
+		WebElement drop = driver.findElement(By.id("droppable"));
+		
+		Actions action = new Actions(driver);
+		
+		//action.clickAndHold(drag).moveToElement(drop).release().build().perform();
+		action.dragAndDrop(drag, drop).perform();
+		
+	//	action.clickAndHold(drop).moveByOffset(200, 0).release().build().perform();
+	}
+
+	public void handleframe() {
+		driver.get("https://jqueryui.com/droppable/");
+		driver.switchTo().frame(0);
+		WebElement drag = driver.findElement(By.id("draggable"));
+		System.out.println(drag.isDisplayed());
+
+		driver.switchTo().defaultContent();
+		System.out.println(driver.findElement(By.xpath("//a[@href='/']")).isDisplayed());
+	}
+
+	public void dropdown() throws InterruptedException {
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+		WebElement dd = driver.findElement(By.name("my-select"));
+		Select select = new Select(dd);
+		System.out.println(select.getFirstSelectedOption().getText());
+		select.selectByVisibleText("Three");
+		Thread.sleep(500);
+		select.selectByValue("1");
+		Thread.sleep(500);
+		select.selectByIndex(2);
+
+		// get all option from the dropdown
+		List<WebElement> alloption = select.getOptions();
+
+		alloption.forEach(x -> {
+			System.out.println(x.getText());
+		});
+
+		System.out.println(select.isMultiple());
+
+		/*
+		 * selectbyindex selectbyvalue selectbyvisibletext ismultiple getoptions
+		 * getfirstselectedoption deselectall deselectbyindex deselectbyvalue
+		 * deselectbyvisible getallselectionoption
+		 */
+
+	}
+
+	public void handleAlert() {
+		driver.get("http://www.tizag.com/javascriptT/javascriptprompt.php");
+		WebElement btn = driver.findElement(By.xpath("//input[@value='Say my name!']"));
+		btn.click();
+		Alert alert = driver.switchTo().alert();
+
+		System.out.println(alert.getText());
+		alert.sendKeys("TestUser");
+		alert.accept();
+		System.out.println(alert.getText());
+		alert.dismiss();
 
 	}
 
